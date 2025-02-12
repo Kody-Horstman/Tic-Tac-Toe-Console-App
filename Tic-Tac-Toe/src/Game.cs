e5ded1a328016bf8236ddcb9d162779068430d6d
Kody Horstman <kchorstman@gmail.com>
2025-02-12 13:00:03 -0500

    Updated Game.startGame method to include a game loop, game menu, and logic to exit the game loop early.

--- Tic-Tac-Toe/src/Game.cs
+++ Tic-Tac-Toe/src/Game.cs
@@ -1,8 +1,30 @@
 namespace TicTacToeConsoleApp {
 
     public class Game {
+        
         public void startGame() {
-            Console.WriteLine("\tStarting game...");
+            bool doGameLoop = true;
+            string menuInput;
+
+            do {
+                // Game menu
+                Console.WriteLine("\n\tNew game started!");
+                Console.WriteLine("\n\t x. squares to be selected.");
+                Console.Write("\n\tEnter a number to make a move or \"exit\" to exit the game: ");
+                menuInput = Console.ReadLine().ToLower();
+
+                switch (menuInput) {
+                    case "exit":
+                        doGameLoop = false;
+                        break;
+                    default:
+                        Console.WriteLine("\tThat wasn't an option.");
+                        break;
+                }
+                
+            } while (doGameLoop);
+
+            Console.WriteLine("\n\tExiting game...\n");
         }
     }
 
