f46421e6d83e09c4856db5f98fe1045d50734436
Kody Horstman <kchorstman@gmail.com>
2025-02-12 13:56:41 -0500

    Updated TicTacToe.Main method to include an application loop; Changed menu options to keep exit handling consistant.

--- Tic-Tac-Toe/src/Program.cs
+++ Tic-Tac-Toe/src/Program.cs
@@ -3,28 +3,30 @@
         
         static void Main() {
             // string menuInput = "";
-            int choice;
+            bool doAppLoop = true;
+            string choice;
             Game game = new();
 
+            do {
+                // Print menu
+                Console.WriteLine("\n\tWelcome to Tic-Tac-Toe!\n");
+                Console.WriteLine("\t 1. Start a game of Tic-Tac-Toe.");
+                Console.Write("\n\tEnter a number or \"exit\": ");
+                choice = Console.ReadLine().ToLower();
+            
 
-            // Print menu
-            Console.WriteLine("\n\tWelcome to Tic-Tac-Toe!\n");
-            Console.WriteLine("\t 1. Start a game of Tic-Tac-Toe.");
-            Console.WriteLine("\t 2. Exit application.");
-            Console.Write("\n\tEnter a number and press ENTER: ");
-            choice = Convert.ToInt32(Console.ReadLine());
-        
-
-            switch (choice) {
-                case 1:
-                    game.startGame();
-                    break;
-                case 2:
-                    break;
-                default:
-                    Console.WriteLine("\tThat didn't work");
-                    break;
-            }
+                switch (choice) {
+                    case "1":
+                        game.startGame();
+                        break;
+                    case "exit":
+                        doAppLoop = false;
+                        break;
+                    default:
+                        Console.WriteLine("\tThe menu was unimpressed...\n");
+                        break;
+                }
+            } while (doAppLoop);
 
             Console.WriteLine("\tExiting. Press any key to continue...");
             Console.Read();
